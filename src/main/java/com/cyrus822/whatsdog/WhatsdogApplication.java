d40f7c0210f4c316ed4d1998e6a8f2d5ccd7554c
Cyrus Cheng <cyrus@cyrus822.com>
2023-10-05 22:52:14 +0800

    add code to api branch

---
+++ src/main/java/com/cyrus822/whatsdog/WhatsdogApplication.java
@@ -0,0 +1,13 @@
+package com.cyrus822.whatsdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsdogApplication.class, args);
+	}
+
+}
