f277e088086ccd4a146136a6bf3fc3cd8e373d22
Cyrus Cheng <cyrus@cyrus822.com>
2023-10-05 22:42:07 +0800

    init the api branch

--- api/src/main/java/com/cyrus822/whatsdog/repos/RestaurantsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cyrus822.whatsdog.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cyrus822.whatsdog.models.Restaurants;
-
-public interface RestaurantsRepo extends JpaRepository<Restaurants, Integer>{
-    
-}
